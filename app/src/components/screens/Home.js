783a4d5aa394d4bca0bfe7512ff3201dd7f079b6
Leon So <leon_so7@hotmail.com>
2021-01-16 02:52:02 -0500

    Changes to home screen

--- app/src/components/screens/Home.js
+++ app/src/components/screens/Home.js
@@ -1,10 +1,32 @@
+import "../../styles/Home.css";
+import Button from 'react-bootstrap/Button'
+import Card from "react-bootstrap/Card";
+import pushupThumbnail from "../../assets/pushup_thumbnail.jpg";
+
 function Home() {
-    return (
+  return (
+    <div>
       <div>
-          <p>Test page</p>
+        <Button variant="primary">Select Exercise</Button>
+        <Button variant="primary">View History</Button>
+        <Button variant="primary">Leaderboard</Button>
       </div>
-    );
-  }
-  
-  export default Home;
-  
+      <div className="TitleDiv">
+        <h className="TitleText">Test page</h>
+      </div>
+      <Card className="bg-dark text-white">
+        <Card.Img src={pushupThumbnail} alt="Pushup Thumbnail" />
+        <Card.ImgOverlay>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+          <Card.Text>Last updated 3 mins ago</Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </div>
+  );
+}
+
+export default Home;
