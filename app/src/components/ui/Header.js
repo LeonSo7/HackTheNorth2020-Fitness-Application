0464e492715cabc3c77085cc311c64c0b85fcafc
Joy Xiao <xiaoz18@mcmaster.ca>
2021-01-16 13:15:04 -0500

    fix header styling

--- app/src/components/ui/Header.js
+++ app/src/components/ui/Header.js
@@ -1,15 +1,15 @@
 import "../../styles/Header.css";
-import { Button, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 function Header() {
   return (
     <div>
       <div>
         <Row>
-          <Col xs lg="2"><div className="topButtons">Select Exercise</div></Col>
-          <Col xs lg="2"><div className="topButtons">View History</div></Col>
-          <Col xs lg="2"><div className="topButtons">Leaderboard</div></Col>
-          <Col><div className="welcome" style={{ textAlign: 'right', padding: '15px' }}> Welcome, Sharon </div></Col>
+          <Col style={{ paddingRight: 0, paddingLeft: '1%', marginLeft: '3%' }} > <div className="topButtons">Select Exercise</div></Col>
+          <Col style={{ paddingRight: 0, paddingLeft: '1%' }} > <div className="topButtons">View History</div></Col>
+          <Col style={{ paddingRight: 0, paddingLeft: '1%' }}><div className="topButtons">Leaderboard</div></Col>
+          <Col xs lg="8"><div className="welcome" style={{ textAlign: 'right', padding: '15px' }}> Welcome, Sharon </div></Col>
         </Row>
       </div>
     </div>
