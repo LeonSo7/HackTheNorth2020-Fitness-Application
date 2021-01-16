ce7557fba6e14dc31c9195a85aaa46711e20fc91
Leon So <leon_so7@hotmail.com>
2021-01-16 12:14:38 -0500

    Home page image cards

--- app/src/components/screens/Home.js
+++ app/src/components/screens/Home.js
@@ -11,67 +11,69 @@ function Home() {
     <div>
       <Header />
       {/* Exercise Cards */}
-      <Col>
-        <CardDeck>
-          <Card className="bg-dark text-white">
-            <Card.Img
-              variant="top"
-              src={pushupThumbnail}
-              alt="Pushup Thumbnail"
-            />
-            <Card.ImgOverlay>
-              <Card.Title>PUSH UPS</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay>
-          </Card>
-          <Card className="bg-dark text-white">
-            <Card.Img src={squatsThumbnail} alt="Squats Thumbnail" />
-            <Card.ImgOverlay>
-              <Card.Title>SQUATS</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay>
-          </Card>
-        </CardDeck>
-        <CardDeck>
-          <Card className="bg-dark text-white">
-            <Card.Img src={situpsThumbnail} alt="Sit Ups Thumbnail" />
-            <Card.ImgOverlay>
-              <Card.Title>SIT UPS</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay>
-          </Card>
-          <Card className="bg-dark text-white">
-            <Card.Img src={lungesThumbnail} alt="Squats Thumbnail" />
-            <Card.ImgOverlay>
-              <Card.Title>PLANKS</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </Card.Text>
-              <Card.Text>Last updated 3 mins ago</Card.Text>
-            </Card.ImgOverlay>
-          </Card>
-        </CardDeck>
-       </Col>
+      <div className="imgContainer">
+        <Col className="imgCol">
+          <CardDeck>
+            <Card className="bg-dark text-white card">
+              <Card.Img
+                variant="top"
+                src={pushupThumbnail}
+                alt="Pushup Thumbnail"
+              />
+              <Card.ImgOverlay>
+                <Card.Title>PUSH UPS</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+            <Card className="bg-dark text-white card">
+              <Card.Img src={squatsThumbnail} alt="Squats Thumbnail" />
+              <Card.ImgOverlay>
+                <Card.Title>SQUATS</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+          </CardDeck>
+          <CardDeck>
+            <Card className="bg-dark text-white card">
+              <Card.Img src={situpsThumbnail} alt="Sit Ups Thumbnail" />
+              <Card.ImgOverlay>
+                <Card.Title>SIT UPS</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+            <Card className="bg-dark text-white card">
+              <Card.Img src={lungesThumbnail} alt="Squats Thumbnail" />
+              <Card.ImgOverlay>
+                <Card.Title>PLANKS</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+                <Card.Text>Last updated 3 mins ago</Card.Text>
+              </Card.ImgOverlay>
+            </Card>
+          </CardDeck>
+        </Col>
+      </div>
 
       <div>
-        <Button variant="primary">Start</Button>
+        <Button variant="primary" className="startBtn">Start</Button>
       </div>
     </div>
   );
