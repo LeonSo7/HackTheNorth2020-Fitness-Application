67e1ef98f67f63f4a10710d66bd493cff1e149b0
Alex Mark <alex.mark@mail.utoronto.ca>
2021-01-17 02:36:49 -0500

    results page

---
+++ app/src/components/screens/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Col, Row } from "react-bootstrap";
+
+class Results extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        }
+    }
+    render() {
+
+    }
+}
