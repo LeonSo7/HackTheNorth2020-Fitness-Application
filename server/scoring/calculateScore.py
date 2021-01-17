3a25b8771d30b29aa7eee46d40f968867eda46b3
Sharon Lin <sharon.lin89@hotmail.com>
2021-01-16 21:33:12 -0500

    Rip no GPU, gotta do some sketch sadge stuffs

--- server/scoring/calculateScore.py
+++ server/scoring/calculateScore.py
@@ -3,6 +3,7 @@ import cv2
 from skimage import measure
 
 # https://www.pyimagesearch.com/2017/06/19/image-difference-with-opencv-and-python/
+# Make sure to greyscale
 def compare_images(img1, img2):
 	image1 = cv2.cvtColor(cv2.imread(img1), cv2.COLOR_BGR2GRAY)
 	image2 = cv2.cvtColor(cv2.imread(img2), cv2.COLOR_BGR2GRAY)
@@ -11,22 +12,24 @@ def compare_images(img1, img2):
 	return score
 
 def compare_workout():
-	youtube_folder = "./data/youtube/youtube_img"
+	youtube_folder = "../vid/youtube_images"
 	youtube_images = sorted([image for image in os.listdir(youtube_folder) if image.endswith(".png")])
 
-	personal_folder = "./data/personal/personal_img"	
+	personal_folder = "../vid/personal_images"	
 	personal_images = sorted([image for image in os.listdir(personal_folder) if image.endswith(".png")])
 
 	cumulative_score = 0
 	total_images = min(len(youtube_images), len(personal_images))
+	total_images_analyzed = 0
 
 	for i in range(total_images):
-		yt_dir = f"{youtube_folder}/{youtube_images[i]}"
-		per_dir = f"{personal_folder}/{personal_images[i]}"
-		print(i)
-		cumulative_score += compare_images(yt_dir, per_dir)
+		if i % 10 == 0:
+			yt_dir = f"{youtube_folder}/{youtube_images[i]}"
+			per_dir = f"{personal_folder}/{personal_images[i]}"
+			cumulative_score += compare_images(yt_dir, per_dir)
+			total_images_analyzed += 1
 
-	return cumulative_score / total_images
+	return str(round(cumulative_score / total_images_analyzed, 2) * 100)
 
-score = compare_workout()
-print(f"Score was {score}")
+# score = compare_workout()
+# print(f"Score was {score}")
