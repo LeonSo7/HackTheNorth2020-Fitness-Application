01041e4bebb2317f1beb52065afd00468c2b6778
Leon So <leon_so7@hotmail.com>
2021-01-17 00:38:24 -0500

    Webcam and video screen changes

--- app/src/components/screens/Workout.js
+++ app/src/components/screens/Workout.js
@@ -1,92 +1,13 @@
 import "../../styles/Workout.css";
 import WebcamStreamCapture from "../webcam/WebcamStreamCapture";
 import React, { Component } from "react";
-// import Webcam from "react-webcam";
-// import { ThemeConsumer } from "react-bootstrap/esm/ThemeProvider";
 
 class Workout extends Component {
-  //   const WebcamComponent = () => <Webcam />;
-  // constructor(props) {
-  //   super(props);
-  //   this.webcamRef = React.createRef();
-  //   this.capturing = false;
-  //   this.mediaRecorderRef = React.createRef();
-  //   this.state = {
-  //     playing: false,
-  //     capturing: false,
-  //     recordedChunks: [],
-  //   };
-  //   this.handleDataAvailable = this.handleDataAvailable.bind(this);
-  //   this.handleStartCaptureClick = this.handleStartCaptureClick.bind(this);
-  //   this.handleStopCaptureClick = this.handleStopCaptureClick.bind(this);
-  //   this.handleDownload = this.handleDownload.bind(this);
-  // }
-
-  // componentDidUpdate() {
-    
-  // }
-
-  // handleDataAvailable = (data) => {
-  //   if (data.size > 0) {
-  //     this.setState({ recordedChunks: this.recordedChunks.concat(data) });
-  //   }
-  // };
-
-  // handleStartCaptureClick = () => {
-  //   this.setState({ capturing: true });
-  //   this.mediaRecorderRef.current = new MediaRecorder(
-  //     this.webcamRef.current.stream,
-  //     {
-  //       mimeType: "video/webm",
-  //     }
-  //   );
-  //   this.mediaRecorderRef.current.addEventListener(
-  //     "dataavailable",
-  //     this.handleDataAvailable
-  //   );
-  //   this.mediaRecorderRef.current.start();
-  // };
-
-  // handleStopCaptureClick = () => {
-  //   this.mediaRecorderRef.current.stop();
-  //   this.setState({ capturing: false });
-  // };
-
-  // // setPlayingState = (newState) => {
-  // //   this.setState(playing, newState);
-  // //   console.log( this.state)
-  // // };
-  // handleDownload = () => {
-  //   if (this.state.recordedChunks.length) {
-  //     const blob = new Blob(this.state.recordedChunks, {
-  //       type: "video/webm",
-  //     });
-  //     const url = URL.createObjectURL(blob);
-  //     const a = document.createElement("a");
-  //     document.body.appendChild(a);
-  //     a.style = "display: none";
-  //     a.href = url;
-  //     a.download = "react-webcam-stream-capture.webm";
-  //     a.click();
-  //     window.URL.revokeObjectURL(url);
-  //     this.setState({recordedChunks: []});
-  //   }
-  // };
 
   render() {
     return (
       <div className="mainContainer">
-        {/* <WebcamStreamCapture playingFunc={this.setPlayingState}/> */}
         <WebcamStreamCapture/>
-        {/* <Webcam audio={false} ref={this.webcamRef} /> */}
-        {/* {this.state.capturing ? (
-          <button onClick={this.handleStopCaptureClick}>Stop Capture</button>
-        ) : (
-          <button onClick={this.handleStartCaptureClick}>Start Capture</button>
-        )}
-        {this.state.recordedChunks.length > 0 && (
-          <button onClick={this.handleDownload}>Download</button>
-        )} */}
       </div>
     );
   }
--- app/src/components/webcam/WebcamStreamCapture.js
+++ app/src/components/webcam/WebcamStreamCapture.js
@@ -1,6 +1,10 @@
 import Webcam from "react-webcam";
 import React from "react";
 import ReactPlayer from "react-player";
+import "../../styles/WebcamStreamCapture.css";
+import axios from "axios";
+import { Row, Col } from "react-bootstrap";
+import Countdown from "react-countdown";
 
 const WebcamStreamCapture = () => {
   const webcamRef = React.useRef(null);
@@ -9,16 +13,19 @@ const WebcamStreamCapture = () => {
   const [capturing, setCapturing] = React.useState(false);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
   const [playState, setPlayState] = React.useState(false);
+  const [duration, setDuration] = React.useState(null);
 
   const handleStartCaptureClick = React.useCallback(() => {
+    // console.log(playerRef);
+    setDuration(playerRef.current.getDuration()*1000);
     setCapturing(true);
     setPlayState(true);
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-    mimeType: "video/webm",
+      mimeType: "video/webm",
     });
     mediaRecorderRef.current.addEventListener(
-    "dataavailable",
-    handleDataAvailable
+      "dataavailable",
+      handleDataAvailable
     );
     mediaRecorderRef.current.start();
   }, [webcamRef, setCapturing, setPlayState, mediaRecorderRef]);
@@ -33,50 +40,101 @@ const WebcamStreamCapture = () => {
   );
 
   const handleStopCaptureClick = React.useCallback(() => {
+    console.log(mediaRecorderRef.current);
     mediaRecorderRef.current.stop();
-    setCapturing(false);
+    // setCapturing(false);
   }, [mediaRecorderRef, webcamRef, setCapturing]);
 
-  const handleDownload = React.useCallback(() => {
+  const handleUpload = () => {
     if (recordedChunks.length) {
       const blob = new Blob(recordedChunks, {
         type: "video/webm",
       });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.webm";
-      a.click();
-      window.URL.revokeObjectURL(url);
+
+      var bodyFormData = new FormData();
+
+      bodyFormData.append("exercise", "pushup");
+      bodyFormData.append("file", blob);
+
+      axios({
+        method: "post",
+        url:
+          "https://cors-anywhere.herokuapp.com/http://35.229.83.24:5000/calculate",
+        data: bodyFormData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then(function (response) {
+          //handle success
+          console.log(response);
+        })
+        .catch(function (response) {
+          //handle error
+          console.log(response);
+        });
+
       setRecordedChunks([]);
     }
-  }, [recordedChunks]);
+  };
 
   return (
     <div>
-      {/* <ReactPlayer ref={playerRef}>
-        <source src={videoSource} />
-        <ControlBar autoHide={true} />
-      </ReactPlayer> */}
-      <div>Test</div>
-      <ReactPlayer 
-        playing={playState} 
-        ref={playerRef} 
-        url={'https://www.youtube.com/watch?v=rUWxSEwctFU&ab_channel=IanRushton'}
-        onEnded={handleStopCaptureClick}
+      <div className="mediaPlayer">
+        <ReactPlayer
+          playing={playState}
+          ref={playerRef}
+          width="60vw"
+          height="60vh"
+          url={
+            "https://www.youtube.com/watch?v=rUWxSEwctFU&ab_channel=IanRushton"
+          }
+          onEnded={handleStopCaptureClick}
         />
-      <Webcam audio={false} ref={webcamRef} />
-      {capturing ? (
-        <button onClick={handleStopCaptureClick}>Stop Capture</button>
-      ) : (
-        // <button onClick={() => {handleStartCaptureClick, props.playingFunc(true)}}>Start Capture</button>
-        <button onClick={handleStartCaptureClick}>Start Capture</button>
-      )}
-      {recordedChunks.length > 0 && (
-        <button onClick={handleDownload}>Download</button>
-      )}
+      </div>
+      <div className="exerciseBtnDiv">
+        <Row>
+          <Col className="buttonCol">
+            <div onClick={() => {}} className="workoutQuitBtn">
+              Quit
+            </div>
+          </Col>
+          <Col className="countDownCol">
+            {/* {console.log(playerRef.getDuration())} */}
+            {duration && (
+              <Countdown date={Date.now() + duration} />
+            )}
+          </Col>
+          <Col className="buttonCol">
+            {!capturing && (
+              <div
+                onClick={handleStartCaptureClick}
+                className="workoutStartBtn"
+              >
+                Start
+              </div>
+            )}
+            {capturing && recordedChunks.length == 0 && (
+              <div className="fillerDiv"></div>
+            )}
+            {recordedChunks.length > 0 && (
+              <div onClick={handleUpload} className="workoutUploadBtn">
+                Upload
+              </div>
+            )}
+          </Col>
+          <Col>
+            <div className="webcamDiv">
+              <Webcam
+                style={{ height: "23vh" }}
+                audio={false}
+                ref={webcamRef}
+              />
+            </div>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
